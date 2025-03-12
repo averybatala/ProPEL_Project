42e0c9b18cc2457216837089781f9a7c65452ffa
averybatala <bataaver0084@apps.hopkinsschools.org>
2025-03-12 20:41:14 +0000

    new practice problem

---
+++ add_nums.py
@@ -0,0 +1,16 @@
+#write a program that takes 2 numbers from the user and asks them 
+num_1 = input("Put your first number here")
+num_2 = #fill in the same as num 1 and remove the hashtag comment
+final_num = 0 #this is a placeholder for the final number you print
+math_type = #create an input asking what math operation the user wants to perform
+
+if math_type == "addition":
+    #final_num = num_1 + num_2
+elif math_type == "subtraction":
+    #do the same but subtract this time
+elif math_type == "multiplication":
+    #do the same but multiply this time
+else:   
+    #do the same but division this time
+
+print(final_num)
