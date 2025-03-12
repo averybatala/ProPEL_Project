35f068a1c06d24d835deeb6a2e3448607d960503
averybatala <bataaver0084@apps.hopkinsschools.org>
2025-03-12 21:58:03 +0000

    git.message

--- add_nums.py
+++ add_nums.py
@@ -1,23 +1,17 @@
 #write a program that takes 2 numbers from the user and asks them 
 # what operation they want to perform, then prints the final result
-num_1 = input("what is your first number?")
-num_2 = input ("what is your second number?")
-final_num =  0 #this is a placeholder for the final number you print
- math_type = num_4 = input(what mach)
+num_1 = int(input("what is your first number? "))
+num_2 = int(input ("what is your second number? "))
+final_num = 0 #this is a placeholder for the final number you print
+math_type = input ("what math operation would you like to preform ") 
 
-# if math_type == "addition":
-#     #final_num = num_1 + num_2
-# elif math_type == "subtraction":
-#     #do the same but subtract this time
-# elif math_type == "multiplication":7
-#     #do the same but multiply this time
-# else:   
-#     #do the same but division this time
+if math_type == ("add"):
+    final_num = num_1 + num_2
+elif math_type == ("sub"):
+    final_num = num_1 - num_2
+elif math_type == ("mult"): 
+     final_num = num_1 * num_2
+else:
+    final_num  = num_1 / num_2
 
-# print(final_num)
-
-
-
-# num1 = input('what is your first number')
-# num2 = input('what is your second number')
-# operation = input('what math do you want to')
+print(final_num)
