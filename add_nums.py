07a9e0e311906bcbad07a3c0742623cf6a131d2b
averybatala <bataaver0084@apps.hopkinsschools.org>
2025-03-12 21:21:28 +0000

    sharing progress

--- add_nums.py
+++ add_nums.py
@@ -1,16 +1,23 @@
 #write a program that takes 2 numbers from the user and asks them 
-num_1 = input("Put your first number here")
-num_2 = #fill in the same as num 1 and remove the hashtag comment
-final_num = 0 #this is a placeholder for the final number you print
-math_type = #create an input asking what math operation the user wants to perform
+# what operation they want to perform, then prints the final result
+num_1 = input("what is your first number?")
+num_2 = input ("what is your second number?")
+final_num =  0 #this is a placeholder for the final number you print
+ math_type = num_4 = input(what mach)
 
-if math_type == "addition":
-    #final_num = num_1 + num_2
-elif math_type == "subtraction":
-    #do the same but subtract this time
-elif math_type == "multiplication":
-    #do the same but multiply this time
-else:   
-    #do the same but division this time
+# if math_type == "addition":
+#     #final_num = num_1 + num_2
+# elif math_type == "subtraction":
+#     #do the same but subtract this time
+# elif math_type == "multiplication":7
+#     #do the same but multiply this time
+# else:   
+#     #do the same but division this time
 
-print(final_num)
+# print(final_num)
+
+
+
+# num1 = input('what is your first number')
+# num2 = input('what is your second number')
+# operation = input('what math do you want to')
