e4ba623fe1790cff92888c746e782853c86d09a8
ResolutePecan <59675913+capwater4@users.noreply.github.com>
2025-03-20 20:04:28 +0000

    creating pandas practice file

---
+++ pandas_nba_practice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#df = pd.read_csv("December_2024.csv")
+
+#df = df[df["PTS"] > 100]
+
+#df.info()
+
+#df.to_csv('December_2024_modified.csv', index=False)
+
+#1. Load January_2025.csv into a dataframe
+#2. Filter to Attend. > 1000 
+#3. Filter to PTS < 100
+#4. Save as January_2025_modified.csv
