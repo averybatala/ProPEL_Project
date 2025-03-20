2f4f6640f3224579d1fc9b72d9ef04848b6de538
averybatala <bataaver0084@apps.hopkinsschools.org>
2025-03-20 21:00:46 +0000

    nba_data

--- pandas_nba_practice.py
+++ pandas_nba_practice.py
@@ -20,3 +20,15 @@ import pandas as pd
 #4. sort by ascending=False for dates
 #5. reset row index
 #6. save your combined csv with an appropriate name
+df_January = pd.read_csv("nba_data/January_2025.csv")
+df_January['Notes'].fillna("No notes", inplace=True)
+
+df_February = pd.read_csv("nba_data/February_2025.csv")
+df_February['Notes'].fillna("No notes", inplace=True)
+
+df_combined = pd.concat([df_January, df_February], ignore_index=True)
+df_combined['Date'] = pd.to_datetime(df_combined['Date'], format='%a %b %d %Y')
+df_combined = df_combined.sort_values(by='Date', ascending=True)
+df_combined.reset_index(drop=True, inplace=True)
+
+df_combined.to_csv('concat_jan_feb.csv', index=False)
