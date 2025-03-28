f2594d9901ab0e50e9b94453e897cdd822ba9030
ResolutePecan <59675913+capwater4@users.noreply.github.com>
2025-03-28 19:46:16 +0000

    reading multiple csv's

--- q1_pandas.py
+++ q1_pandas.py
@@ -1,10 +1,19 @@
 #Which visitor team scored the most points?
 #To get the maximum value of a specific column, use df['column_name'].max()
+#
 import pandas as pd
+import glob
 
-df_q1_pandas = pd.read_csv("nba_data/November_2024.csv")
-#df = df_q1_pandas['PTS_Visitor'].max()
+# Use glob to find all CSV files in the directory
+csv_files = glob.glob("nba_data/*.csv")
 
-max_value = df_q1_pandas['PTS_Visitor'].max()
-row_with_max = df_q1_pandas[df_q1_pandas['PTS_Visitor'] == max_value]
+# Read all CSV files into a list of DataFrames
+df_list = [pd.read_csv(file) for file in csv_files]
+
+# Concatenate all DataFrames into a single DataFrame
+df_all = pd.concat(df_list, ignore_index=True)
+
+max_value = df_all['PTS_Visitor'].max()
+row_with_max = df_all[df_all['PTS_Visitor'] == max_value]
 print(row_with_max)
+
